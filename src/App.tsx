d77628e25007d550b32ff36d88db09eb5caf6744
Andrii Orap <andrii.orap@latoken.com>
2022-04-18 00:04:56 +0200

    add markers by click

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import L, { Icon } from "leaflet";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
 
 const visitorIcon = L.icon({
   iconSize: [25, 41],
@@ -12,37 +13,36 @@ const visitorIcon = L.icon({
   shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
 });
 
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
+function LocationMarkers() {
+  const initialMarkers: any[] = [];
+  const [markers, setMarkers] = useState(initialMarkers);
 
-  const map = useMap();
-
-  useEffect(() => {
-    map.locate().on("locationfound", function (e:any) {
-      setPosition(e.latlng);
+  const map = useMapEvents({
+    click: (e) => setMarkers([...markers, e.latlng]),
+    locationfound: (e) => {
+      setMarkers([...markers])
       map.flyTo(e.latlng, map.getZoom());
-    });
-  }, []);
-
-  return position === null ? null : (
-    <Marker position={position} icon={visitorIcon}>
-      <Popup>You are here</Popup>
-    </Marker>
+    },
+  });
+  return (
+    <React.Fragment>
+      {markers.map((position, idx) => <Marker position={position} icon={visitorIcon} key={`marker-${idx}`}> </Marker> )}
+    </React.Fragment>
   );
 }
 
 function App() {
   return (
     <MapContainer 
-      center={[45.4, -75.7]} 
+      center={[50.166258, 19.9415741]} 
       zoom={12}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <LocationMarkers />
 
-      <LocationMarker />
     </MapContainer>
   );
 }
